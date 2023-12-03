1cffa38c37a36856608b28544b3af8d3a4a28cfe
Facundo Wade <wadefacundo@gmail.com>
2023-12-03 17:08:21 -0300

    added solution to day 1 part 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text.RegularExpressions;
+
+var lines = File.ReadLines("input.txt");
+
+var sum = lines
+    .Select(line => new Regex(@"\d").Matches(line))
+    .Select(matches => int.Parse($"{matches[0].Value}{matches[^1].Value}"))
+    .Sum();
+
+Console.WriteLine(sum);
